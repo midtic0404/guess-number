eb06f4447841bc16d20849de0c07c37dff459b50
Chia Che Chang <midtic0404@gmail.com>
2020-05-13 23:22:23 -0600

    Added basic game logic and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [progressText, setProgressText] = useState(`Let's Play`);
+  const [answer, setAnswer] = useState(0);
+  const [gameOn, setGameOn] = useState(false);
+  const [userInput, setUserInput] = useState('');
+
+  function startGame() {
+    const newRandomNumber = Math.floor(Math.random() * 100) + 1;
+    setProgressText('Enter A Number :)');
+    setAnswer(newRandomNumber);
+    setGameOn(true);
+  }
+
+  function checkAnswer() {
+    if (inputValid(userInput)) {
+      const userAnswer = +userInput;
+      if (userAnswer > answer) {
+        setProgressText(`Lower Than ${userAnswer}`);
+      } else if (userAnswer < answer) {
+        setProgressText(`Higher Than ${userAnswer}`);
+      } else {
+        setProgressText(`Yes! ${answer}`);
+        setGameOn(false);
+      }
+      setUserInput('');
+    } else {
+      console.log('Invalid Input');
+    }
+  }
+
+  function inputValid(input) {
+    if (input.trim() === '' || isNaN(+input)) {
+      return false;
+    }
+    return true;
+  }
+
+  function onUserInput(e) {
+    setUserInput(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Guess Number Between 1 to 100</h2>
+
+      <div className='progress'>
+        <h1>{progressText}</h1>
+      </div>
+
+      {gameOn ? (
+        <div>
+          <input
+            className='inputBox'
+            type='text'
+            value={userInput}
+            onChange={onUserInput}
+          />
+          <button className='button' onClick={checkAnswer}>
+            Guess
+          </button>
+        </div>
+      ) : (
+        <button className='button' onClick={startGame}>
+          Start New Game
+        </button>
+      )}
     </div>
   );
 }
